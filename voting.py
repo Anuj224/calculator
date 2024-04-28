7d996ff794200c52d05e451b86563ee335922d27
anuj shahdeo <168348938+Anuj224@users.noreply.github.com>
2024-04-28 19:38:38 +0530

    Add files via upload
    basic voting

---
+++ voting.py
@@ -0,0 +1,13 @@
+def vote_yours():
+        a = input("choose your voting party: ")
+        if a == "BJP":
+                print(f"THANKS FOR VOTING!! YOUR VOTE IS ALLOTTED TO {a}",)
+        elif a == "CONGRESS":
+                print(f"THANKS FOR VOTING!! YOUR VOTE IS ALLOTTED TO {a}")
+        elif a == "AAP":
+                print(f" THANKS FOR VOTING!! YOUR VOTE IS ALLOTTED TO {a}")
+        elif a == "SP":
+                print(f"THANKS FOR VOTING!! YOUR VOTE IS ALLOTTED TO {a}")
+        else:
+                print(f"THANKS FOR VOTING!! YOUR VOTE IS ALLOTTED TO {a}")
+vote_yours()
